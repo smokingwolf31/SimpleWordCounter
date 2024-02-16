5b12d7529b071bb511cb5738a68e04d6c3b9200a
mnqobi <nkbmnq002@myuct.ac.za>
2024-02-16 21:54:30 +0200

    Fixed the bug which was making the program print the incorect number of lines

---
+++ Makefile
@@ -0,0 +1,13 @@
+G=g++
+
+myWC.exe: analyser.o main.o
+	$(G) analyser.o main.o -o myWC.exe -std=c++20
+
+%.o: %.cpp
+	$(G) -c $< -o $@
+
+run:
+	./driver.exe
+
+clean:
+	rm *.o myWC.exe
---
+++ analyser.h
@@ -0,0 +1,12 @@
+#ifndef ANAYLSER_H
+#define ANALYSER_H
+	namespace NKBMNQ002{
+		struct ResultBuilder{
+			int words, chars;
+		};
+
+		bool isValidChar(char yChar);
+
+		void lineAnalyser(char* currentLine, ResultBuilder& currentWordsAndChars);
+	}
+#endif
