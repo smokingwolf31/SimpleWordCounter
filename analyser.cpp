a4de6e4b9f426a1d75d8af59091911396a72c034
mnqobi <nkbmnq002@myuct.ac.za>
2024-02-16 19:33:20 +0200

    Added the analyser.cpp file while has an unfinished implimitation of the lineAnalyser function(currently instended to count the number of words and chars given a string)

---
+++ analyser.cpp
@@ -0,0 +1,9 @@
+#include "analyser.h"
+
+void NKBMNQ002::lineAnalyser(char* currentLine, ResultBuilder& currentNumberOfWordsAndChars){
+	using namespace NKBMNQ002;
+	int index=0;
+	while(currentLine[index] != '\n' && currentLine[index] != '\0'){
+		
+	}
+}
